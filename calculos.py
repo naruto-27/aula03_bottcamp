cdeb237f22b91cb299d48ae155d035cf16e566be
Naruto-27 <marciana272009@hotmail.com>
2024-09-26 20:45:21 -0300

    calculo fatorial

---
+++ calculos.py
@@ -0,0 +1,9 @@
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
+
+# Testando a função
+print(fatorial(5))  # Saída: 120
+print(fatorial(0))  # Saída: 1
