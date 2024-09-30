8b2372b321800a667bf8270dfdc5240184abeeec
Naruto-27 <marciana272009@hotmail.com>
2024-09-30 19:19:05 -0300

    aprimorando exercicios

--- calculos.py
+++ calculos.py
@@ -9,3 +9,15 @@ print(fatorial(5))  # Saída: 120
 print(fatorial(0))  # Saída: 1
 
 
+
+
+def ordenar_lista(lista):
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            if lista[i] > lista[j]:
+                lista[i], lista[j] = lista[j], lista[i]
+    return lista
+
+# Testando a função
+print(ordenar_lista([34, 2, 25, 6, 12]))  # Saída: [2, 6, 12, 25, 34]
+print(ordenar_lista([5, 3, 8, 1, 2]))    # Saída: [1, 2, 3, 5, 8]
